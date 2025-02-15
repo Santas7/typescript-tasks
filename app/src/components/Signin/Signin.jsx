18fb591ece65d32c385f17363a7728035c706020
Santas7 <andreypanyshkin03@gmail.com>
2025-02-15 15:08:58 +0400

    upd components

--- app/src/components/Signin/Signin.jsx
+++ app/src/components/Signin/Signin.jsx
@@ -1,7 +1,12 @@
-export function Signin() {
+export function Signin({ onSubmit }) {
     return (
         <div>
-            <h1>Signin</h1>
+            <h2>Раздел Signin</h2>
+            <form onSubmit={onSubmit}>
+                <input type="email" placeholder="Введите Email" />
+                <input type="password" placeholder="Введите пароль" />
+                <button type="submit">Войти</button>
+            </form>
         </div>
     )
 }
--- app/src/components/Signup/Signup.jsx
+++ app/src/components/Signup/Signup.jsx
@@ -1,7 +1,24 @@
-export function Signup() {
+export function Signup( { onSubmit } ) {
     return (
         <div>
-            <h1>Signup</h1>
+            <h2>Раздел Signup</h2>
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="Введите имя" />
+                <input type="text" placeholder="Введите ник"/>
+                <input type="email" placeholder="Введите Email" />
+                <span>Пол:</span>
+                <label>
+                    <input type="radio" required />
+                    <span>Мужской</span>
+                </label>
+                <label>
+                    <input type="radio" required />
+                    <span>Женский</span>
+                </label>
+
+                <input type="password" placeholder="Введите пароль" />
+                <button type="submit">Зарегистрироваться</button>
+            </form>
         </div>
     )
 }
