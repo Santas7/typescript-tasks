159dcef5dc8de58be5acdd45e4cade84a7a3eeb1
Santas7 <andreypanyshkin03@gmail.com>
2025-03-13 11:53:26 +0400

    upd

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,7 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', 
+      manifest: {
+        name: 'App',
+        short_name: 'App',
+        description: 'Test App (React)',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192x192.png', 
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png', 
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg}'],
+      },
+    }),
+  ],
+});
