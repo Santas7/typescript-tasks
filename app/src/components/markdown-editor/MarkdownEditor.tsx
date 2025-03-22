218731e7561f1b8d0dab9892123b3e6770da0a97
Santas7 <andreypanyshkin03@gmail.com>
2025-03-22 20:10:13 +0400

    upd

--- app/src/components/markdown-editor/MarkdownEditor.tsx
+++
@@ -1,8 +0,0 @@
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
-import { MarkdownEditorProps } from "../../types/types-interfaces";
-
-
-export default function MarkdownEditor({ value, onChange }: MarkdownEditorProps) {
-  return <SimpleMDE value={value} onChange={onChange} />;
-}
