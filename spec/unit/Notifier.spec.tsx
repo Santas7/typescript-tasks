f24e7ad5fa4f1a835525052e598edfa4c0ab81de
Santas7 <andreypanyshkin03@gmail.com>
2025-04-29 17:46:52 +0400

    upd

--- spec/unit/Notifier.spec.tsx
+++ spec/unit/Notifier.spec.tsx
@@ -19,17 +19,11 @@ describe('Окно оповещения', () => {
     it('автоматически исчезает с экрана через 2 секунды', () => {
         const onClose = jest.fn();
         const taskName = "Любая задача";
-
         const { rerender } = render(<Notifier open={true} task={taskName} onClose={onClose} />);
-
         expect(screen.getByText(taskName)).toBeInTheDocument();
-
         jest.advanceTimersByTime(2000);
-
         expect(onClose).toHaveBeenCalledTimes(1);
-
         rerender(<Notifier open={false} task={taskName} onClose={onClose} />);
-
         expect(screen.queryByText(taskName)).not.toBeInTheDocument();
     });
 });
